0b79151f399d46eb173c11ac479d42bc630b9f82
priyanshi <priyanshikaramchandani1526@gmail.com>
2024-03-18 20:04:34 +0530

    feat: add tailwindCSS and materialUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "./output.css"
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { themeOptions } from './theme.js'
+
+const theme = createTheme(themeOptions)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const themeOptions = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: 'rgba(255,255,255,0)',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        background: {
+            paper: '#5013ff',
+        },
+    },
+};
